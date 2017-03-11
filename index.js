f2ec5fade0bfee12f637638a396e5e03580d8930
Spencer Brown <spencer@mixmax.com>
2017-03-10 17:30:57 -0800

    Require that `options.localDependencies` is not empty

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ class Erik {
    * @param {String[]} [options.taskDependencies] - Names of tasks to be run before any Erik
    * processing is done. Useful for registering your spec-building/processing tasks as dependencies
    * of Erik's testing task. These tasks will be run in serial as passed.
-   * @param {String[]} [options.localDependencies] - Local dependencies to be bundled alongisde your
-   * remote dependencies. Glob strings.
+   * @param {String[]} options.localDependencies - Local dependencies to be bundled alongisde your
+   * remote dependencies. Should include your specs. Glob strings.
    * @param {String[]} [options.remoteDependencies] - URLs corresponding to remote dependencies.
    * @param {Object} [options.karmaConfig]
    * @param {Number} [options.karmaConfig.port=9876] - Port on which to run the Karma server.
@@ -72,6 +72,10 @@ class Erik {
 
     if (!Array.isArray(this._localDependencies)) {
       throw new Error('`options.localDependencies` is of an invalid type.');
+    }
+
+    if (this._localDependencies.length === 0) {
+      throw new Error('`options.localDependencies` is empty.');
     }
 
     if (!Array.isArray(this._remoteDependencies)) {
